ade4cb06ae024f6e652dca3e15a5c2077d395a0e
Bill Lou <billloukehao@gmail.com>
2019-07-08 21:29:09 -0700

    Implement more tests for tictactoe
    - Tests for board initialization
    - Tests for playing pieces
    - Tests for switching players
    - Tests for winning a game as either player

--- test/test.js
+++ test/test.js
@@ -6,12 +6,93 @@ describe('TicTacToe game', function () {
     const ticTacToe = new ticTacToeClass();
 
     it('should create a board string', function () {
-      assert.strictEqual(ticTacToe.boardStr, '   |   |   \n---+---+---\n   |   |   \n---+---+---\n   |   |   ');
+      assert.strictEqual('   |   |   \n---+---+---\n   |   |   \n---+---+---\n   |   |   ', ticTacToe.boardStr);
     });
 
     it('should create a 3x3 board array of 0s', function () {
       const boardArr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
       assert.deepEqual(boardArr, ticTacToe.boardArr);
     });
+  });
+
+  describe('playing pieces', function () {
+    const ticTacToe = new ticTacToeClass();
+
+    it('should update board with "X" after playing first piece', function () {
+      ticTacToe.playPiece('1');
+      assert.strictEqual(' X |   |   \n---+---+---\n   |   |   \n---+---+---\n   |   |   ', ticTacToe.boardStr);
+      assert.deepEqual([['X', 0, 0], [0, 0, 0], [0, 0, 0]], ticTacToe.boardArr);
+    });
+
+    it('should update board with "O" after playing second piece', function () {
+      ticTacToe.playPiece('2');
+      assert.strictEqual(' X | O |   \n---+---+---\n   |   |   \n---+---+---\n   |   |   ', ticTacToe.boardStr);
+      assert.deepEqual([['X', 'O', 0], [0, 0, 0], [0, 0, 0]], ticTacToe.boardArr);
+    });
+
+    it('should not update board if playing piece on location already taken', function () {
+      ticTacToe.playPiece('2');
+      ticTacToe.playPiece('1');
+      ticTacToe.playPiece('2');
+      assert.strictEqual(' X | O |   \n---+---+---\n   |   |   \n---+---+---\n   |   |   ', ticTacToe.boardStr);
+      assert.deepEqual([['X', 'O', 0], [0, 0, 0], [0, 0, 0]], ticTacToe.boardArr);
+    });
+
+    it('should not update board if playing non-valid keys', function () {
+      ticTacToe.playPiece('x');
+      ticTacToe.playPiece('!');
+      assert.strictEqual(' X | O |   \n---+---+---\n   |   |   \n---+---+---\n   |   |   ', ticTacToe.boardStr);
+      assert.deepEqual([['X', 'O', 0], [0, 0, 0], [0, 0, 0]], ticTacToe.boardArr);
+    });
+  });
+
+  describe('swapping players automatically', function () {
+    const ticTacToe = new ticTacToeClass();
+
+    it('should start with player X', function () {
+      assert.strictEqual('X', ticTacToe.player);
+    });
+
+    it('should change to player O after playing a piece', function () {
+      ticTacToe.playPiece('2');
+      assert.strictEqual('O', ticTacToe.player);
+    });
+
+    it('should change to player X after playing another piece', function () {
+      ticTacToe.playPiece('1');
+      assert.strictEqual('X', ticTacToe.player);
+    });
+
+    it('should not change players if playing piece on a location already taken', function () {
+      ticTacToe.playPiece('1');
+      ticTacToe.playPiece('2');
+      assert.strictEqual('X', ticTacToe.player);
+    });
+  });
+
+  describe('winning a game', function () {
+    it('row win for player X', function () {
+      const ticTacToe = new ticTacToeClass();
+      ticTacToe.playPiece('1');
+      ticTacToe.playPiece('4');
+      ticTacToe.playPiece('2');
+      ticTacToe.playPiece('5');
+      ticTacToe.playPiece('3');
+      assert.strictEqual('X', ticTacToe.player);
+      assert.equal(true, ticTacToe.checkWin());
+    });
+
+    it('row win for player O', function () {
+      const ticTacToe = new ticTacToeClass();
+      ticTacToe.playPiece('1');
+      ticTacToe.playPiece('4');
+      ticTacToe.playPiece('2');
+      ticTacToe.playPiece('5');
+      ticTacToe.playPiece('9');
+      ticTacToe.playPiece('6');
+      assert.strictEqual('O', ticTacToe.player);
+      assert.equal(true, ticTacToe.checkWin());
+    });
+
   });
 });
